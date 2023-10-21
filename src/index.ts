a2302f36d3ca3c7fc737dc9f9ee68cc9c2eb4f7e
Irina Dinescu <irina.dinescu27@gmail.com>
2023-10-21 12:27:44 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //npm install typescript del-cli --save-dev
 //npm version patch
-//Re-export stuff from errors and middlewares 
+//Re-export stuff from errors and middlewares
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
@@ -12,3 +12,9 @@ export * from './middleware/current-user';
 export * from './middleware/error-handler';
 export * from './middleware/require-auth';
 export * from './middleware/validate-request';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
