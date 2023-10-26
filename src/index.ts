4ecc02a33dae7d4364aef92f76f451b42c3bb716
Irina Dinescu <irina.dinescu27@gmail.com>
2023-10-26 22:55:12 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,4 @@ export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
-export * from '../src/events/types/order-status';
+export * from './events/types/order-status';
